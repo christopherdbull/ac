46ca4442fa05390b20c2ec96e6367394c64b9ea5
James Wigglesworth <james+mbp2011@pocketsmith.com>
2013-07-13 17:51:58 +1200

    angry client repo init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gatherlove::Application.config.secret_token = 'b3bde352380b64780e69ebf1e0b2e5455add1cbffc53928daddffe961265ea4c423ad306e2f4f7bb3af58a594c1f6607e5aa0eb7f0e471dc0cd793946d8e0b41'
